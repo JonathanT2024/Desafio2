e65aec116e2e85d9581e91119cb69251f2d435e6
JonathanT2024 <jonathan.ingenieriaindustrial@gmail.com>
2024-08-16 11:58:31 -0400

    instalacion base postgres

--- desafio2/settings.py
+++ desafio2/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'desafio2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'desafio2',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+
     }
 }
 
